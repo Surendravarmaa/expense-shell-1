9bc1e394f0e5627836080ba9ca54021c9580b9e8
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-07-18 11:26:29 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -5,22 +5,22 @@ source ./common.sh
 checkout
 
 dnf install mysql-server -y &>>$LOGFILE
-#VALIDATE $? "Installing MySQL Server"
+VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-#VALIDATE $? "Enabling MySQL Server"
+VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-#VALIDATE $? "Starting MySQL Server"
+VALIDATE $? "Starting MySQL Server"
 
 mysql -h db.surendra.online -uroot -p${MYSQL_ROOT_PASSWORD} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
     mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>$LOGFILE
-    #VALIDATE $? "MySQL Root password setup"
+    VALIDATE $? "MySQL Root password setup"
 else
     echo -e "Already mysql root password is set...$Y SKIPPING $N"
 fi
 
-#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-#VALIDATE $? "Setting up root password"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting up root password"
